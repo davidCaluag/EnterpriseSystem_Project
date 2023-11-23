5a0030f4fa33080120a461e83d022498239fe2a5
mattie129 <mattiehagan129@gmail.com>
2023-11-23 09:57:06 -0500

    Created Album class

---
+++ Models/Album.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_EnterpriseSystem.Models
+{
+    public class Album
+    {
+        public Guid Id {get; set; }
+        public required string Name {get; set; }
+        public List<Song>? Songs { get; set; }
+
+    }
+}
