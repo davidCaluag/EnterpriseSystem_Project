64d82463568e56e93a2f0da4e6479927437ff4a5
David Caluag <davidcaluag2000@gmail.com>
2023-11-29 17:42:26 -0500

    Finalized User Controller...

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Project_EnterpriseSystem.Models;
 using Project_EnterpriseSystem.Services;
 
 namespace Project_EnterpriseSystem.Controllers
@@ -15,11 +16,61 @@ namespace Project_EnterpriseSystem.Controllers
         private UserDatabase database = new();
 
         [HttpGet]
-
         public async Task<IActionResult> GetAllUsers(){
 
             var userList = await database.Users.ToListAsync();
             return Ok(userList);    
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> AddUser(User newUser){
+            
+            if(newUser == default){
+                throw new ArgumentNullException("Default parameters for user...");
+            }
+
+            if(newUser.userName == default)
+                throw new ArgumentNullException("Username is empty (REQUIRED)");
+
+            var person = await database.Users.FindAsync(newUser.Username);
+            if(person != default){
+                throw new ArgumentOutOfRangeException("Username already in the database...");
+            }
+
+            await database.Users.AddAsync(newUser);
+            await database.SaveChangesAsync();
+
+            return Created("Created", newUser);
+        }
+
+        [HttpGet("/{userName}")]
+
+        public async Task<IActionResult> FindUser(string userName){
+
+            var person =  await database.Users.FindAsync(userName);
+
+            if(person == default)
+                throw new ArgumentOutOfRangeException("User not found...");
+            
+            var DTO = new{
+                Name = person.Username,
+                PlayListCount = person.ListOfPlaylists.Count,
+                Id = person.Id
+            };
+            return Ok(DTO);
+        }
+
+        [HttpDelete("/{userName}")]
+        public async Task<IActionResult> DeleteUser(string userName){
+            var person =  await database.Users.FindAsync(userName);
+
+            if(person == default)
+                throw new ArgumentOutOfRangeException("User not found...");
+            
+            database.Users.Remove(person);
+
+            return Ok($"Deleted {userName}");
+            
         }
     }
 }
