16ceb1efebb94cf288e69cbf4746f742bb1b5597
Lucas Busetto <busettol@sheridancollege.ca>
2023-12-10 11:57:09 -0500

    Added browse controller and basic functionality

---
+++ Controllers/BrowseController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnterpriseSystem_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Project_EnterpriseSystem.Services;
+
+namespace EnterpriseSystem_Project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BrowseController: ControllerBase
+    {
+        private UserDatabase database = new();
+
+        //get all songs
+        [HttpGet("/allSongs")]
+        public async Task<IActionResult> GetAllSongs(){
+            //if no songs are in the database throw an exception
+            if (database.Songs.Count() == 0){
+                throw new Exception("No songs in database");
+            }
+            var songs = database.Songs;
+            return Ok(songs);
+        }
+
+
+
+        //get songs with a specific genre
+        [HttpGet("/allGenres/{genre}")]
+        public async Task<IActionResult> GetSongsByGenre(Genre genre){
+
+            //scrape Songs in database where the Genre matches the argument given
+            var searchGenre = database.Songs.Where(x => x.Genre == genre);
+            return Ok(searchGenre);
+        }
+    }
+}
